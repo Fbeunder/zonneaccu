208d11f6cd2e0d219022144b916f8246c22499d5
Fbeunder <frank@lynxx.eu>
2025-05-07 13:32:23 +0200

    Add storage_modules package initialization

---
+++ storage_modules/__init__.py
@@ -0,0 +1,8 @@
+"""Package voor verschillende energieopslagmodules.
+
+Dit package bevat modules voor het berekenen van de effectiviteit en rendabiliteit
+van verschillende energieopslagmethoden zoals:
+- Warmwaterboilers
+- Accu's/Batterijen
+- Andere opslagmethoden die in de toekomst kunnen worden toegevoegd
+"""
