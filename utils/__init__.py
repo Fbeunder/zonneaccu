02a88e0e32ac3227cf8ecdfbae5fff7068457798
Fbeunder <frank@lynxx.eu>
2025-05-07 13:32:44 +0200

    Add utils package initialization

---
+++ utils/__init__.py
@@ -0,0 +1,8 @@
+"""Package voor algemene hulpfuncties en utilities.
+
+Dit package bevat modules voor:
+- Configuratiebeheer
+- Herbruikbare hulpfuncties
+- Constanten en defaults
+- Andere algemene functionaliteit die door meerdere componenten wordt gebruikt
+"""
