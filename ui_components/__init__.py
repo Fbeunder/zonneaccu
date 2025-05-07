bd61f291c8d247fadf92a15e75663e024d3d71ed
Fbeunder <frank@lynxx.eu>
2025-05-07 16:32:59 +0200

    Update ui_components/__init__.py om boiler_analysis_page te importeren

--- ui_components/__init__.py
+++ ui_components/__init__.py
@@ -1,6 +1,8 @@
 """UI componenten voor de Zonneaccu applicatie."""
 
 from ui_components.data_import_page import render_data_import_page
+from ui_components.data_analysis_page import render_data_analysis_page
+from ui_components.boiler_analysis_page import render_boiler_analysis_page
 
 # Niet direct importeren om circulaire imports te voorkomen
 # De modules worden direct geïmporteerd in app.py
