ea15637775d840747c733f87250b21e853d45904
Fbeunder <frank@lynxx.eu>
2025-05-07 15:59:42 +0200

    Update ui_components/__init__.py

--- ui_components/__init__.py
+++ ui_components/__init__.py
@@ -2,4 +2,5 @@
 
 from ui_components.data_import_page import render_data_import_page
 
-__all__ = ['render_data_import_page']
+# Niet direct importeren om circulaire imports te voorkomen
+# De modules worden direct geïmporteerd in app.py
